48ca9688d59ac944f24b158c71f05e5d7a6ada04
KimHyunse <anzmf12@naver.com>
2018-10-14 12:18:17 -0400

    Set up Front / Back End

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+        Hi CapPic
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ server/src/index.ts
@@ -0,0 +1,11 @@
+import app from './app';
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  return console.log(`server is listening on ${port}`);
+});
