569abdcb1cc825aee23b71dd664d4cf82ed102eb
KimHyunse <anzmf12@naver.com>
2018-10-15 18:31:04 -0400

    install styled-components/ components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Main from './Components/Main';
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        Hi CapPic
+        <Main />
       </div>
     );
   }
---
+++ client/src/Components/Main/Main.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  background-color: #dfe6e9;
+`;
+
+class Main extends Component {
+  public render() {
+    return <Container>Camera</Container>;
+  }
+}
+
+export default Main;
