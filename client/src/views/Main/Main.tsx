f5c71704eafd51a58d36632bb2727bbe2eddeb3b
KimHyunse <anzmf12@naver.com>
2018-10-17 22:25:36 -0400

    Add Start & Stop Button

--- client/src/views/Main/Main.tsx
+++ client/src/views/Main/Main.tsx
@@ -43,7 +43,6 @@ const Button = styled.button`
     }
   }};
 `;
-
 // E - Styled Component
 
 class Main extends Component {
@@ -51,48 +50,54 @@ class Main extends Component {
   private cameraOutput: any;
   private canvas: any;
 
-  public componentDidMount() {
-    const cameraOuput = this.cameraOutput;
-
-    // Assign Tracker to this.tracker
-    // Object is face
-    this.tracker = new (window as any).tracking.ObjectTracker('face');
-    this.tracker.setInitialScale(4);
-    this.tracker.setStepSize(2);
-    this.tracker.setEdgesDensity(0.1);
-
-    /**
-     * window should be widow as any because of typescript....
-     */
-    (window as any).tracking.track(cameraOuput, this.tracker, {
-      camera: true
-    });
-    this.tracker.on('track', (event) => {
-      const canvas: any = this.canvas;
-      const context = canvas.getContext('2d');
-      context.clearRect(0, 0, canvas.width, canvas.height);
-
-      event.data.forEach((rect) => {
-        context.strokeStyle = '#a64ceb';
-        context.strokeRect(rect.x, rect.y, rect.width, rect.height);
-        context.font = '11px Helvetica';
-        context.fillStyle = '#fff';
-        context.fillText(
-          'x: ' + rect.x + 'px',
-          rect.x + rect.width + 5,
-          rect.y + 11
-        );
-        context.fillText(
-          'y: ' + rect.y + 'px',
-          rect.x + rect.width + 5,
-          rect.y + 22
-        );
-      });
-    });
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  public componentWillUnmount() {
-    this.tracker.removeAllListeners();
+  public handleClick({ currentTarget: { name } }) {
+    const cameraOuput = this.cameraOutput;
+
+    switch (name) {
+      case 'start':
+        this.tracker = new (window as any).tracking.ObjectTracker('face');
+        this.tracker.setInitialScale(4);
+        this.tracker.setStepSize(2);
+        this.tracker.setEdgesDensity(0.1);
+
+        (window as any).tracking.track(cameraOuput, this.tracker, {
+          camera: true
+        });
+        this.tracker.on('track', (event) => {
+          const canvas: any = this.canvas;
+          const context = canvas.getContext('2d');
+          context.clearRect(0, 0, canvas.width, canvas.height);
+
+          event.data.forEach((rect) => {
+            context.strokeStyle = '#a64ceb';
+            context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+            context.font = '11px Helvetica';
+            context.fillStyle = '#fff';
+            context.fillText(
+              'x: ' + rect.x + 'px',
+              rect.x + rect.width + 5,
+              rect.y + 11
+            );
+            context.fillText(
+              'y: ' + rect.y + 'px',
+              rect.x + rect.width + 5,
+              rect.y + 22
+            );
+          });
+        });
+        break;
+      case 'stop':
+        this.tracker.removeAllListeners();
+        this.cameraOutput.pause();
+        break;
+      default:
+        break;
+    }
   }
 
   public render() {
@@ -106,8 +111,12 @@ class Main extends Component {
           loop={true}
         />
         <Canvas ref={(ref) => (this.canvas = ref)} width="640" height="480" />
-        <Button name="start">Start</Button>
-        <Button name="stop">Stop</Button>
+        <Button name="start" onClick={this.handleClick}>
+          Start
+        </Button>
+        <Button name="stop" onClick={this.handleClick}>
+          Stop
+        </Button>
       </Container>
     );
   }
